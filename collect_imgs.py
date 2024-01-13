561c3923e646c2b98c0aafef57b9a79b666fd171
zayedu <umerzayed1@gmail.com>
2024-01-13 14:49:30 -0500

    Working on collect_imgs

--- collect_imgs.py
+++ collect_imgs.py
@@ -3,3 +3,10 @@ import os
 import cv2
 
 DATA_dir = './data'
+if not os.path.exists(DATA_DIR):
+    os.makedirs(DATA_DIR)
+    
+number_of_classes = 3
+dataset_size = 100
+
+cap = cv2.VideoCapture(1)

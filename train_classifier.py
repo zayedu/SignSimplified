f13aa9f3599b04075f39886603817c53798328d9
zayedu <umerzayed1@gmail.com>
2024-01-13 20:06:07 -0500

    Created file to train model

---
+++ train_classifier.py
@@ -0,0 +1,7 @@
+import pickle
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+import numpy as np
+

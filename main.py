8411ccd09fa2a6b017eaae13806a555f38b690e7
zayedu <umerzayed1@gmail.com>
2024-01-13 20:32:53 -0500

    Added Dictionary to classify Characters

---
+++ main.py
@@ -0,0 +1,18 @@
+import pickle
+
+import cv2
+import mediapipe as mp
+import numpy as np
+
+model_dict = pickle.load(open('./train_model/model.p', 'rb'))
+model = model_dict['model']
+
+cap = cv2.VideoCapture(1)
+
+mp_hands = mp.solutions.hands
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+
+hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
+
+labels_dict = {0: 'A', 1: 'B', 2: 'L'}

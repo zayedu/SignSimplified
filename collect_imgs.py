c967094df6bfb03577ce83e7dde4ca964b5359f7
zayedu <umerzayed1@gmail.com>
2024-01-13 19:49:17 -0500

    added images for training ML Model

--- collect_imgs.py
+++ collect_imgs.py
@@ -1,11 +1,12 @@
-import os 
+import os
 
 import cv2
 
-DATA_dir = './data'
+
+DATA_DIR = './data'
 if not os.path.exists(DATA_DIR):
     os.makedirs(DATA_DIR)
-    
+
 number_of_classes = 3
 dataset_size = 100
 

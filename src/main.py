f81df14ba5d940b33cd659ecdba66e374ff8d4dd
zayedu <umerzayed1@gmail.com>
2024-01-14 22:02:12 -0500

    Updated ReadMe

--- src/main.py
+++ src/main.py
@@ -13,9 +13,6 @@ mp_hands = mp.solutions.hands
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 
-hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
-
-labels_dict = {0: 'A', 1: 'B', 2: 'D', 3: 'F',4:'Frick'}
 
 while True:
 
--- src/train_model/collect_imgs.py
+++ src/train_model/collect_imgs.py
@@ -1,3 +1,4 @@
+#Reach out at umerz@mcmaster.ca
 import os
 
 import cv2
@@ -7,7 +8,7 @@ DATA_DIR = './data'
 if not os.path.exists(DATA_DIR):
     os.makedirs(DATA_DIR)
 
-number_of_classes = 4
+number_of_classes = 3
 dataset_size = 100
 
 cap = cv2.VideoCapture(1)
